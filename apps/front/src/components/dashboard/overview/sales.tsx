51e530da04996dc4009fb67e239082b973d31a5c
breathoftravel <breath.of.travels@gmail.com>
2024-09-20 04:50:02 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ apps/front/src/components/dashboard/overview/sales.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardHeader from '@mui/material/CardHeader';
+import Divider from '@mui/material/Divider';
+import type { SxProps } from '@mui/material/styles';
+import { ArrowClockwise as ArrowClockwiseIcon } from '@phosphor-icons/react/dist/ssr/ArrowClockwise';
+import { ArrowRight as ArrowRightIcon } from '@phosphor-icons/react/dist/ssr/ArrowRight';
+
+export interface SalesProps {
+  sx?: SxProps;
+}
+
+export function Sales({ sx }: SalesProps): React.JSX.Element {
+
+  return (
+    <Card sx={sx}>
+      <CardHeader
+        action={
+          <Button color="inherit" size="small" startIcon={<ArrowClockwiseIcon fontSize="var(--icon-fontSize-md)" />}>
+            Sync
+          </Button>
+        }
+        title="Sales"
+      />
+      <Divider />
+      <CardActions sx={{ justifyContent: 'flex-end' }}>
+        <Button color="inherit" endIcon={<ArrowRightIcon fontSize="var(--icon-fontSize-md)" />} size="small">
+          Overview
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
